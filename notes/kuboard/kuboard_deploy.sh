58fe6b6c131d71bc66135006be2701311508669e
BigDEM0N <2678757810@qq.com>
2024-09-14 17:26:53 +0800

    <feature>增加kuboard部署note

---
+++ notes/kuboard/kuboard_deploy.sh
@@ -0,0 +1,9 @@
+sudo docker run -d \
+  --restart=unless-stopped \
+  --name=kuboard \
+  -p 80:80/tcp \
+  -p 10081:10081/tcp \
+  -e KUBOARD_ENDPOINT="http://10.0.12.11:80" \
+  -e KUBOARD_AGENT_SERVER_TCP_PORT="10081" \
+  -v /root/kuboard-data:/data \
+  swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard:v3
