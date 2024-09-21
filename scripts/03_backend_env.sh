5a60594595c4c2cf2faa844795eff0141a73ea6b
BigDEM0N <2678757810@qq.com>
2024-09-21 23:31:23 +0800

    feature: 增加创建容器功能

--- scripts/03_backend_env.sh
+++ scripts/03_backend_env.sh
@@ -2,6 +2,8 @@ rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
 curl -s https://mirror.go-repo.io/centos/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
 yum install golang
 
+yum install docker-compose
+
 cd ./mysql || exit
 docker-compose -f mysql-init.yaml up
 cd ..
