1b28eae263ef60136ba68c4bfe9e64aae265ce03
BigDEM0N <2678757810@qq.com>
2024-09-21 23:16:47 +0800

    feature: 增加初始化数据库脚本

---
+++ scripts/03_backend_env.sh
@@ -0,0 +1,14 @@
+rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
+curl -s https://mirror.go-repo.io/centos/go-repo.repo | tee /etc/yum.repos.d/go-repo.repo
+yum install golang
+
+cd ./mysql || exit
+docker-compose -f mysql-init.yaml up
+cd ..
+
+cd ../server/ || exit
+
+# 设置go install 代理
+go env -w GOPROXY=https://goproxy.cn,direct
+# 下载依赖
+go mod tidy
